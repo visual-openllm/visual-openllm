fe9b267dd0a1133776f23717ac8df7ed68e1d426
PeterDing <dfhayst@gmail.com>
2023-03-26 20:11:52 +0800

    Remove instruction prompt

--- visual_openllm/llm.py
+++ visual_openllm/llm.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 from peft import PeftModel
 from transformers import AutoTokenizer
@@ -6,7 +8,7 @@ from .chatglm.modeling_chatglm import ChatGLMForConditionalGeneration
 
 
 class LLM:
-    PROMPT_FORMAT = """Instruction: {input_text}\nAnswer: """
+    # PROMPT_FORMAT = """Instruction: {input_text}\nAnswer: """
 
     def __init__(self):
         torch.set_default_tensor_type(torch.cuda.HalfTensor)
@@ -15,11 +17,12 @@ class LLM:
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
         self.tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
 
-    def call(self, input_text: str) -> str:
+    def __call__(self, input_texts: List[str]) -> List[str]:
+        input_text = input_texts[0]
         with torch.no_grad():
-            input_text = self.PROMPT_FORMAT.format(input_text=input_text)
+            # input_text = self.PROMPT_FORMAT.format(input_text=input_text)
             ids = self.tokenizer.encode(input_text)
             input_ids = torch.LongTensor([ids])
             out = self.model.generate(input_ids=input_ids, max_length=150, do_sample=False, temperature=0)
             out_text = self.tokenizer.decode(out[0])
-            return out_text
+            return [out_text]
