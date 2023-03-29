39908b65d02d19367fd44eeea872120e7576af26
PeterDing <dfhayst@gmail.com>
2023-03-29 18:25:11 +0800

    Insist on putting model to cuda

--- visual_openllm/llm.py
+++ visual_openllm/llm.py
@@ -41,8 +41,10 @@ class LLM:
     PROMPT_FORMAT = """Instruction: {input_text}\nAnswer: """
 
     def __init__(self):
+        self.device = "cuda:0"
         torch.set_default_tensor_type(torch.cuda.HalfTensor)
         self.model = ChatGLMForConditionalGeneration.from_pretrained("THUDM/chatglm-6b", device_map="auto")
+        self.model.to(self.device)
         self.model = PeftModel.from_pretrained(self.model, "visual-openllm/visual-openllm-chatglm-6b-rola")
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
         self.tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
@@ -60,9 +62,9 @@ class LLM:
         with torch.no_grad():
             format_input = self.PROMPT_FORMAT.format(input_text=input)
             ids = self.tokenizer.encode(format_input)
-            input_ids = torch.LongTensor([ids])
+            input_ids = torch.LongTensor([ids]).to(self.device)
             out = self.model.generate(input_ids=input_ids, max_length=512, do_sample=False, temperature=0)
-            out_text = self.tokenizer.decode(out[0][len(ids) :])
+            out_text = self.tokenizer.decode(out[0].detach()[len(ids) :])
             result = self.prompt_parser.parse_output(out_text)
             if result:
                 action, action_input = result
